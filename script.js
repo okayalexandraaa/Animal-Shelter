0be387c5f20bcef346ade38b3d3a8db05723308d
okayalexandraaa <43342086+okayalexandraaa@users.noreply.github.com>
2019-05-21 18:14:46 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,17 @@
+$("#cat").click(function() {
+    $("#cat").fadeOut();
+});
+
+$("#dog").click(function() {
+    $("#dog").fadeOut();
+});
+
+$("#feraligatr").click(function() {
+    $("#feraligatr").fadeOut();
+});
+
+$("#send-back").click(function() {
+    $("#cat").show();
+    $("#dog").show();
+    $("#feraligatr").show();
+});
